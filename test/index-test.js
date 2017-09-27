5fdc0b10b8ed75499e041d8742dcc7bb3f545d7c
Tracy Fernandez <tr.ferna@gmail.com>
2017-09-27 00:35:28 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -5,7 +5,7 @@ beforeEach(function() {
   b = Math.floor(Math.random() * 1000)
 })
 
-it('add(a, b) adds two numbers and returns the result', function add81(a, b) {
+it('add(a, b) adds two numbers and returns the result', function add81() {
   expect(add(a, b)).toEqual(a + b)
 })
 
